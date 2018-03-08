5eba8ba99799b84b41ad3d4fd12732665064183f
rpizarrog <rpizarro@itdurango.edu.mx>
2018-03-07 19:49:59 -0600

    Add files via upload

---
+++ agragar estados insert.sql
@@ -0,0 +1,41 @@
+/* Agregraando registros a las tablas de tienda
+Primero agregar estados de la Rep Mexicana, algunos ...
+Luego municipios de lo sestados de algunos ...
+Finalmente alguanas tiendas con nombre ficticios que se encuentran en algunos municipios 
+*/
+
+-- estados
+insert into ESTADOS (estado, nombre) values (1,'Aguascalientes');
+insert into ESTADOS (estado, nombre) values (2,'Baja California');
+insert into ESTADOS (estado, nombre) values (3,'Baja California Sur');
+insert into ESTADOS (estado, nombre) values (4,'Campeche');
+insert into ESTADOS (estado, nombre) values (5,'Coahuila de Zaragoza');
+insert into ESTADOS (estado, nombre) values (6,'Colima');
+insert into ESTADOS (estado, nombre) values (7,'Chiapas');
+insert into ESTADOS (estado, nombre) values (8,'Chihuahua');
+insert into ESTADOS (estado, nombre) values (9,'Distrito Federal');
+-- insert into ESTADOS (estado, nombre) values (10,'Durango'); -- quitar -- para insertar
+insert into ESTADOS (estado, nombre) values (11,'Guanajuato');
+insert into ESTADOS (estado, nombre) values (12,'Guerrero');
+insert into ESTADOS (estado, nombre) values (13,'Hidalgo');
+insert into ESTADOS (estado, nombre) values (14,'Jalisco');
+insert into ESTADOS (estado, nombre) values (15,'México');
+insert into ESTADOS (estado, nombre) values (16,'Michoacán de Ocampo');
+insert into ESTADOS (estado, nombre) values (17,'Morelos');
+insert into ESTADOS (estado, nombre) values (18,'Nayarit');
+insert into ESTADOS (estado, nombre) values (19,'Nuevo León');
+insert into ESTADOS (estado, nombre) values (20,'Oaxaca');
+insert into ESTADOS (estado, nombre) values (21,'Puebla');
+insert into ESTADOS (estado, nombre) values (22,'Querétaro');
+insert into ESTADOS (estado, nombre) values (23,'Quintana Roo');
+insert into ESTADOS (estado, nombre) values (24,'San Luis Potosí');
+insert into ESTADOS (estado, nombre) values (25,'Sinaloa');
+insert into ESTADOS (estado, nombre) values (26,'Sonora');
+insert into ESTADOS (estado, nombre) values (27,'Tabasco');
+insert into ESTADOS (estado, nombre) values (28,'Tamaulipas');
+insert into ESTADOS (estado, nombre) values (29,'Tlaxcala');
+insert into ESTADOS (estado, nombre) values (30,'Veracruz de Ignacio de la Llave');
+insert into ESTADOS (estado, nombre) values (31,'Yucatán');
+-- insert into ESTADOS (estado, nombre) values (32,'Zacatecas'); -- quitar -- para insertar
+    
+
